7aa4af034d73bdee41f1deed2e2c9558fca6a49f
Tom Sauer <tom@exluto.com>
2017-10-13 19:09:14 -0400

    Uploading Base Project Files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+
+	// Use this for initialization
+	void Update () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 250f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 20f;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+		
+	}
+}
+	
